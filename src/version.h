a381eb23a3955d402b6c6607adbcbb3e7cdd31c5
Michael Smith <mikesmiffy128@gmail.com>
2022-04-24 04:24:20 +0100

    Preemptively bump version to 0.3
    No idea when the next release will happen yet, but this might as well
    happen now to avoid forgetting later.

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 2
-#define VERSION "0.2"
+#define VERSION_MINOR 3
+#define VERSION "0.3"
