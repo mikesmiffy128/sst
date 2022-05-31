1eff445a270a422ba94ede3205be739e998e530f
Michael Smith <mikesmiffy128@gmail.com>
2022-05-31 20:38:31 +0100

    Bump version number and zip dates
    Preparing for release, I spose!

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
-#define VERSION "0.3"
+#define VERSION_MINOR 4
+#define VERSION "0.4"
