37afe67488ed93dc5e39942271dd24481797fe77
Michael Smith <mikesmiffy128@gmail.com>
2022-06-02 16:03:34 +0100

    Bump version number right away (from now on!)
    It'll be less confusing not to have development builds versioned the
    same as existing releases. That's probably obvious, but I'm still new to
    this releasing software business!

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 4
-#define VERSION "0.4"
+#define VERSION_MINOR 5
+#define VERSION "0.5"
