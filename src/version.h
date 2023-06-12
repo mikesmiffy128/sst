19fe683f14e1ba71e34b18d8f753bb5c93a75126
Michael Smith <mikesmiffy128@gmail.com>
2023-06-12 22:39:37 +0100

    Bump version to 0.6 and reset changelog string

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 5
-#define VERSION "0.5"
+#define VERSION_MINOR 6
+#define VERSION "0.6"
