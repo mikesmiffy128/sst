a35365c3e8e291aa870d580cd64e8de807319c3e
Michael Smith <mikesmiffy128@gmail.com>
2024-08-26 14:15:11 +0100

    Bump version to 0.8 for next release

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 7
-#define VERSION "0.7"
+#define VERSION_MINOR 8
+#define VERSION "0.8"
