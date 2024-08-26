45a2f22f4ef7e251b0183e5bae71ad12967a9f52
Michael Smith <mikesmiffy128@gmail.com>
2024-08-26 22:09:50 +0100

    Begin the new version cycle yet again at 0.9

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 8
-#define VERSION "0.8"
+#define VERSION_MINOR 9
+#define VERSION "0.9"
